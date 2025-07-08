fff556407bf6c15abaf550cc06702f87d18025e6
picoCTF <picoCTF@gmail.com>
2025-07-09 01:08:31 +0700

    feedback commit

--- components/timeline-demo.tsx
+++ components/timeline-demo.tsx
@@ -3,26 +3,51 @@ import { Timeline } from "@/components/ui/timeline";
 
 export default function TimelineDemo() {
   const data = [
-     {
-       title: "2023",
-       content: (
-        <div>
-            <h1><b>Gadget Repair</b></h1>
+    {
+      title: "2025",
+      content: (
+       <div>
+            <h1><b> Enterprise Risk Management Web Tool Using OCTAVE Allegro and MISP for Threat
+            Intelligence</b></h1>
             <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-            Collaborated on designing and implementing an interactive web project to enhance user
-            engagement through dynamic event handling and forms. Utilized HTML, CSS, JavaScript,
-            and jQuery to create a seamless and responsive user experience.
+            Provide a Structured Risk Assessment Framework - Implement OCTAVE Allegro
+            methodology to enhance cybersecurity risk management.
             </p>
             <div className="grid grid-cols-2 gap-4">
             <img
-                src="/assets/portfolio/3.png"
+                src="/assets/portfolio/11.png"
                 alt="Project screenshot 1"
                 width={500}
                 height={500}
                 className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-                src="/assets/portfolio/4.png"
+                src="/assets/portfolio/12.png"
+                alt="Project screenshot 2"
+                width={500}
+                height={500}
+                className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+            />
+            </div>
+
+            <br />
+
+            <h1><b>Secure Sign App</b></h1>
+            <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+            The core goal of the SecureSign application is to ensure the safety and privacy of 
+            documents uploaded by users. This is achieved through robust encryption techniques 
+            that protect the documents from unauthorized access.
+            </p>
+            <div className="grid grid-cols-2 gap-4">
+            <img
+                src="/assets/portfolio/13.png"
+                alt="Project screenshot 1"
+                width={500}
+                height={500}
+                className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+            />
+            <img
+                src="/assets/portfolio/14.png"
                 alt="Project screenshot 2"
                 width={500}
                 height={500}
@@ -30,7 +55,7 @@ export default function TimelineDemo() {
             />
             </div>
         </div>
-        ),
+      ),
     },
     {
       title: "2024",
@@ -134,50 +159,25 @@ export default function TimelineDemo() {
       ),
     },
     {
-      title: "2025",
-      content: (
-       <div>
-            <h1><b> Enterprise Risk Management Web Tool Using OCTAVE Allegro and MISP for Threat
-            Intelligence</b></h1>
+       title: "2023",
+       content: (
+        <div>
+            <h1><b>Gadget Repair</b></h1>
             <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-            Provide a Structured Risk Assessment Framework - Implement OCTAVE Allegro
-            methodology to enhance cybersecurity risk management.
+            Collaborated on designing and implementing an interactive web project to enhance user
+            engagement through dynamic event handling and forms. Utilized HTML, CSS, JavaScript,
+            and jQuery to create a seamless and responsive user experience.
             </p>
             <div className="grid grid-cols-2 gap-4">
             <img
-                src="/assets/portfolio/11.png"
+                src="/assets/portfolio/3.png"
                 alt="Project screenshot 1"
                 width={500}
                 height={500}
                 className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-                src="/assets/portfolio/12.png"
-                alt="Project screenshot 2"
-                width={500}
-                height={500}
-                className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            </div>
-
-            <br />
-
-            <h1><b>Secure Sign App</b></h1>
-            <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
-            The core goal of the SecureSign application is to ensure the safety and privacy of 
-            documents uploaded by users. This is achieved through robust encryption techniques 
-            that protect the documents from unauthorized access.
-            </p>
-            <div className="grid grid-cols-2 gap-4">
-            <img
-                src="/assets/portfolio/13.png"
-                alt="Project screenshot 1"
-                width={500}
-                height={500}
-                className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-                src="/assets/portfolio/14.png"
+                src="/assets/portfolio/4.png"
                 alt="Project screenshot 2"
                 width={500}
                 height={500}
@@ -185,7 +185,7 @@ export default function TimelineDemo() {
             />
             </div>
         </div>
-      ),
+        ),
     },
   ];
 
